5a68c0e48fbb0abb6f5d45fdc133f454d1986f31
wrtv <155278230+wrtv@users.noreply.github.com>
2025-08-16 17:40:34 +0800

    Initial commit

---
+++ function/xiaomi_sports/xiaomi.py
@@ -0,0 +1,381 @@
+# -*- coding: utf8 -*-
+# python >=3.8
+
+import random
+import re
+import sys
+from urllib.parse import quote
+
+sys.path.append("My-Actions/function/wps")
+from sendNotify import *
+
+#设置中国时区+8
+if os.environ.get('OS_TZ'):
+    os.environ['TZ'] = 'UTC' + str(-int(os.environ['OS_TZ'])) + 'CN'
+    time.tzset()
+else:
+    os.environ['TZ'] = 'UTC-8CN'
+    time.tzset()
+
+sendNotify = sendNotify()
+now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+headers = {
+    'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 9; MI 6 MIUI/20.6.18)'
+}
+
+
+# 获取登录code
+def get_code(location):
+    code_pattern = re.compile("(?<=access=).*?(?=&)")
+    code = code_pattern.findall(location)[0]
+    return code
+
+
+# 登录
+
+
+def login(_user, password, type):
+    if type:
+        url1 = "https://api-user.huami.com/registrations/+86" + _user + "/tokens"
+    else:
+        url1 = "https://api-user.huami.com/registrations/" + _user + "/tokens"
+
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_7_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2"
+    }
+    data1 = {
+        "client_id": "HuaMi",
+        "password": f"{password}",
+        "redirect_uri": "https://s3-us-west-2.amazonaws.com/hm-registration/successsignin.html",
+        "token": "access"
+    }
+    r1 = requests.post(url1, data=data1, headers=headers, allow_redirects=False)
+    try:
+        location = r1.headers["Location"]
+        code = get_code(location)
+    except:
+        return 0, 0
+    # print(f'access_code获取成功！ {code}')
+
+    url2 = "https://account.huami.com/v2/client/login"
+    if type:
+        data2 = {
+        "allow_registration=": "false",
+        "app_name": "com.xiaomi.hm.health",
+        "app_version": "6.3.5",
+        "code": f"{code}",
+        "country_code": "CN",
+        "device_id": "2C8B4939-0CCD-4E94-8CBA-CB8EA6E613A1",
+        "device_model": "phone",
+        "grant_type": "access_token",
+        "third_name": "email" if "@" in _user else "huami_phone",
+        }
+    else:
+        data2 = {
+        "allow_registration=": "false",
+        "app_name": "com.xiaomi.hm.health",
+        "app_version": "6.3.5",
+        "code": f"{code}",
+        "country_code": "CN",
+        "device_id": "2C8B4939-0CCD-4E94-8CBA-CB8EA6E613A1",
+        "device_model": "phone",
+        "dn": "api-user.huami.com%2Capi-mifit.huami.com%2Capp-analytics.huami.com",
+        "grant_type": "access_token",
+        "lang": "zh_CN",
+        "os_version": "1.5.0",
+        "source": "com.xiaomi.hm.health",
+        "third_name": "email",
+        }
+
+    r2 = requests.post(url2, data=data2, headers=headers).json()
+    login_token = r2["token_info"]["login_token"]
+    # print("login_token获取成功！")
+    # print(login_token)
+    userid = r2["token_info"]["user_id"]
+    # print("userid获取成功！")
+    # print(userid)
+
+    return login_token, userid
+
+
+# 主函数
+def main(_user, _passwd, _step):
+    if '@' in _user:
+        _type = False
+    else:
+        _type = True
+    _user = str(_user)
+    _passwd = str(_passwd)
+    _step = str(_step)
+
+    if _step == '':
+        print("已设置为随机步数（10000-39999）")
+        _step = str(random.randint(10000, 39999))
+
+    login_token, userid = login(_user, _passwd, _type)
+
+    if login_token == 0:
+        print("登陆失败！")
+        if os.environ.get('SEND_KEY'):
+            sendNotify.send(title="小米运动自动刷步数", msg="【小米运动自动刷步数】\n登陆失败！")
+        return "login fail!"
+
+    t = get_time()
+
+    app_token = get_app_token(login_token)
+
+    today = time.strftime("%F")
+
+    summary = {
+        "v": 6,
+        "slp": {
+            "st": int(time.time()),
+            "ed": int(time.time()),
+            "dp": 0,
+            "lt": 0,
+            "wk": 0,
+            "usrSt": -1440,
+            "usrEd": -1440,
+            "wc": 0,
+            "is": 0,
+            "lb": 0,
+            "to": 0,
+            "dt": 0,
+            "rhr": 0,
+            "ss": 0
+        },
+        "stp": {
+            "ttl": _step,
+            "dis": 10627,
+            "cal": 510,
+            "wk": 41,
+            "rn": 50,
+            "runDist": 7654,
+            "runCal": 397,
+            "stage": [
+                {
+                    "start": 327,
+                    "stop": 341,
+                    "mode": 1,
+                    "dis": 481,
+                    "cal": 13,
+                    "step": 680
+                },
+                {
+                    "start": 342,
+                    "stop": 367,
+                    "mode": 3,
+                    "dis": 2295,
+                    "cal": 95,
+                    "step": 2874
+                },
+                {
+                    "start": 368,
+                    "stop": 377,
+                    "mode": 4,
+                    "dis": 1592,
+                    "cal": 88,
+                    "step": 1664
+                },
+                {
+                    "start": 378,
+                    "stop": 386,
+                    "mode": 3,
+                    "dis": 1072,
+                    "cal": 51,
+                    "step": 1245
+                },
+                {
+                    "start": 387,
+                    "stop": 393,
+                    "mode": 4,
+                    "dis": 1036,
+                    "cal": 57,
+                    "step": 1124
+                },
+                {
+                    "start": 394,
+                    "stop": 398,
+                    "mode": 3,
+                    "dis": 488,
+                    "cal": 19,
+                    "step": 607
+                },
+                {
+                    "start": 399,
+                    "stop": 414,
+                    "mode": 4,
+                    "dis": 2220,
+                    "cal": 120,
+                    "step": 2371
+                },
+                {
+                    "start": 415,
+                    "stop": 427,
+                    "mode": 3,
+                    "dis": 1268,
+                    "cal": 59,
+                    "step": 1489
+                },
+                {
+                    "start": 428,
+                    "stop": 433,
+                    "mode": 1,
+                    "dis": 152,
+                    "cal": 4,
+                    "step": 238
+                },
+                {
+                    "start": 434,
+                    "stop": 444,
+                    "mode": 3,
+                    "dis": 2295,
+                    "cal": 95,
+                    "step": 2874
+                },
+                {
+                    "start": 445,
+                    "stop": 455,
+                    "mode": 4,
+                    "dis": 1592,
+                    "cal": 88,
+                    "step": 1664
+                },
+                {
+                    "start": 456,
+                    "stop": 466,
+                    "mode": 3,
+                    "dis": 1072,
+                    "cal": 51,
+                    "step": 1245
+                },
+                {
+                    "start": 467,
+                    "stop": 477,
+                    "mode": 4,
+                    "dis": 1036,
+                    "cal": 57,
+                    "step": 1124
+                },
+                {
+                    "start": 478,
+                    "stop": 488,
+                    "mode": 3,
+                    "dis": 488,
+                    "cal": 19,
+                    "step": 607
+                },
+                {
+                    "start": 489,
+                    "stop": 499,
+                    "mode": 4,
+                    "dis": 2220,
+                    "cal": 120,
+                    "step": 2371
+                },
+                {
+                    "start": 500,
+                    "stop": 511,
+                    "mode": 3,
+                    "dis": 1268,
+                    "cal": 59,
+                    "step": 1489
+                },
+                {
+                    "start": 512,
+                    "stop": 522,
+                    "mode": 1,
+                    "dis": 152,
+                    "cal": 4,
+                    "step": 238
+                }
+            ]
+        },
+        "goal": 15000,
+        "tz": "28800"
+    }
+
+    data_json = [
+        {
+            "data_hr": "//////9L////////////Vv///////////0v///////////9e/////0n/a///S////////////0b//////////1FK////////////R/////////////////9PTFFpaf9L////////////R////////////0j///////////9K////////////Ov///////////zf///86/zr/Ov88/zf/Pf///0v/S/8/////////////Sf///////////z3//////0r/Ov//////S/9L/zb/Sf9K/0v/Rf9H/zj/Sf9K/0//N////0D/Sf83/zr/Pf9M/0v/Ov9e////////////S////////////zv//z7/O/83/zv/N/83/zr/N/86/z//Nv83/zn/Xv84/zr/PP84/zj/N/9e/zr/N/89/03/P/89/z3/Q/9N/0v/Tv9C/0H/Of9D/zz/Of88/z//PP9A/zr/N/86/zz/Nv87/0D/Ov84/0v/O/84/zf/MP83/zH/Nv83/zf/N/84/zf/Of82/zf/OP83/zb/Mv81/zX/R/9L/0v/O/9I/0T/S/9A/zn/Pf89/zn/Nf9K/07/N/83/zn/Nv83/zv/O/9A/0H/Of8//zj/PP83/zj/S/87/zj/Nv84/zf/Of83/zf/Of83/zb/Nv9L/zj/Nv82/zb/N/85/zf/N/9J/zf/Nv83/zj/Nv84/0r/Sv83/zf/MP///zb/Mv82/zb/Of85/z7/Nv8//0r/S/85/0H/QP9B/0D/Nf89/zj/Ov83/zv/Nv8//0f/Sv9O/0ZeXv///////////1X///////////9B////////////TP///1b//////0////////////9N/////////v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+",
+            "date": today,
+            "data": [
+                {
+                    "start": 0,
+                    "stop": 1439,
+                    "value": "UA8AUBQAUAwAUBoAUAEAYCcAUBkAUB4AUBgAUCAAUAEAUBkAUAwAYAsAYB8AYB0AYBgAYCoAYBgAYB4AUCcAUBsAUB8AUBwAUBIAYBkAYB8AUBoAUBMAUCEAUCIAYBYAUBwAUCAAUBgAUCAAUBcAYBsAYCUAATIPYD0KECQAYDMAYB0AYAsAYCAAYDwAYCIAYB0AYBcAYCQAYB0AYBAAYCMAYAoAYCIAYCEAYCYAYBsAYBUAYAYAYCIAYCMAUB0AUCAAUBYAUCoAUBEAUC8AUB0AUBYAUDMAUDoAUBkAUC0AUBQAUBwAUA0AUBsAUAoAUCEAUBYAUAwAUB4AUAwAUCcAUCYAUCwKYDUAAUUlEC8IYEMAYEgAYDoAYBAAUAMAUBkAWgAAWgAAWgAAWgAAWgAAUAgAWgAAUBAAUAQAUA4AUA8AUAkAUAIAUAYAUAcAUAIAWgAAUAQAUAkAUAEAUBkAUCUAWgAAUAYAUBEAWgAAUBYAWgAAUAYAWgAAWgAAWgAAWgAAUBcAUAcAWgAAUBUAUAoAUAIAWgAAUAQAUAYAUCgAWgAAUAgAWgAAWgAAUAwAWwAAXCMAUBQAWwAAUAIAWgAAWgAAWgAAWgAAWgAAWgAAWgAAWgAAWREAWQIAUAMAWSEAUDoAUDIAUB8AUCEAUC4AXB4AUA4AWgAAUBIAUA8AUBAAUCUAUCIAUAMAUAEAUAsAUAMAUCwAUBYAWgAAWgAAWgAAWgAAWgAAWgAAUAYAWgAAWgAAWgAAUAYAWwAAWgAAUAYAXAQAUAMAUBsAUBcAUCAAWwAAWgAAWgAAWgAAWgAAUBgAUB4AWgAAUAcAUAwAWQIAWQkAUAEAUAIAWgAAUAoAWgAAUAYAUB0AWgAAWgAAUAkAWgAAWSwAUBIAWgAAUC4AWSYAWgAAUAYAUAoAUAkAUAIAUAcAWgAAUAEAUBEAUBgAUBcAWRYAUA0AWSgAUB4AUDQAUBoAXA4AUA8AUBwAUA8AUA4AUA4AWgAAUAIAUCMAWgAAUCwAUBgAUAYAUAAAUAAAUAAAUAAAUAAAUAAAUAAAUAAAUAAAWwAAUAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAeSEAeQ8AcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcBcAcAAAcAAAcCYOcBUAUAAAUAAAUAAAUAAAUAUAUAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcCgAeQAAcAAAcAAAcAAAcAAAcAAAcAYAcAAAcBgAeQAAcAAAcAAAegAAegAAcAAAcAcAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcCkAeQAAcAcAcAAAcAAAcAwAcAAAcAAAcAIAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcCIAeQAAcAAAcAAAcAAAcAAAcAAAeRwAeQAAWgAAUAAAUAAAUAAAUAAAUAAAcAAAcAAAcBoAeScAeQAAegAAcBkAeQAAUAAAUAAAUAAAUAAAUAAAUAAAcAAAcAAAcAAAcAAAcAAAcAAAegAAegAAcAAAcAAAcBgAeQAAcAAAcAAAcAAAcAAAcAAAcAkAegAAegAAcAcAcAAAcAcAcAAAcAAAcAAAcAAAcA8AeQAAcAAAcAAAeRQAcAwAUAAAUAAAUAAAUAAAUAAAUAAAcAAAcBEAcA0AcAAAWQsAUAAAUAAAUAAAUAAAUAAAcAAAcAoAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAYAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcBYAegAAcAAAcAAAegAAcAcAcAAAcAAAcAAAcAAAcAAAeRkAegAAegAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAEAcAAAcAAAcAAAcAUAcAQAcAAAcBIAeQAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcBsAcAAAcAAAcBcAeQAAUAAAUAAAUAAAUAAAUAAAUBQAcBYAUAAAUAAAUAoAWRYAWTQAWQAAUAAAUAAAUAAAcAAAcAAAcAAAcAAAcAAAcAMAcAAAcAQAcAAAcAAAcAAAcDMAeSIAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcAAAcBQAeQwAcAAAcAAAcAAAcAMAcAAAeSoAcA8AcDMAcAYAeQoAcAwAcFQAcEMAeVIAaTYAbBcNYAsAYBIAYAIAYAIAYBUAYCwAYBMAYDYAYCkAYDcAUCoAUCcAUAUAUBAAWgAAYBoAYBcAYCgAUAMAUAYAUBYAUA4AUBgAUAgAUAgAUAsAUAsAUA4AUAMAUAYAUAQAUBIAASsSUDAAUDAAUBAAYAYAUBAAUAUAUCAAUBoAUCAAUBAAUAoAYAIAUAQAUAgAUCcAUAsAUCIAUCUAUAoAUA4AUB8AUBkAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAAfgAA",
+                    "tz": 32,
+                    "did": "DA932FFFFE8816E7",
+                    "src": 24
+                }
+            ],
+            "summary": json.dumps(summary),
+            "source": 24,
+            "type": 0
+        }
+    ]
+
+    url = f'https://api-mifit-cn.huami.com/v1/data/band_data.json?&t={t}'
+    head = {
+        "apptoken": app_token,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    data = f'userid={userid}&last_sync_data_time=1597306380&device_type=0&last_deviceid=DA932FFFFE8816E7&data_json={quote(json.dumps(data_json))} '
+
+    response = requests.post(url, data=data, headers=head).json()
+    # print(response)
+    result = f"{user[:4]}****{user[-4:]}: [{now}] 修改步数（{step}）" + response['message']
+    print(result)
+    return result
+
+
+# 获取时间戳
+def get_time():
+    url = "https://f.m.suning.com/api/ct.do"
+    response = requests.get(url, headers=headers).json()
+    t = response["currentTime"]
+    return t
+
+
+# 获取app_token
+def get_app_token(login_token):
+    url = f"https://account-cn.huami.com/v1/client/app_tokens?app_name=com.xiaomi.hm.health&dn=api-user.huami.com%2Capi-mifit.huami.com%2Capp-analytics.huami.com&login_token={login_token}"
+    response = requests.get(url, headers=headers).json()
+    app_token = response['token_info']['app_token']
+    # print(f'app_token获取成功 {app_token}')
+    return app_token
+
+
+if __name__ == "__main__":
+    # 用户名（格式为 13800138000）
+    if os.environ.get('Xiaomi_User') and os.environ.get('Xiaomi_Pw'):
+        # 用户名（格式为 13800138000）
+        user = os.environ['Xiaomi_User']
+        # 登录密码
+        passwd = os.environ['Xiaomi_Pw']
+    else:
+        print("未填写小米运动账号或密码取消运行")
+        exit(0)
+
+    # 要修改的步数，直接输入想要修改的步数值，留空为随机步数
+    if os.environ.get('Xiaomi_Bs'):
+        step = os.environ['Xiaomi_Bs'].replace('[', '').replace(']', '')
+
+    user_list = user.split('#')
+    passwd_list = passwd.split('#')
+    step_array = step.split('-')
+
+    if len(user_list) == len(passwd_list):
+        push = ''
+        for line in range(0, len(user_list)):
+            if len(step_array) == 2:
+                step = str(random.randint(int(step_array[0]), int(step_array[1])))
+            elif str(step) == '0':
+                step = ''
+            push += main(user_list[line], passwd_list[line], step) + '\n'
+        if not os.environ.get('SEND_KEY'):
+            sendNotify.send(title="小米运动自动刷步数", msg="【小米运动自动刷步数】\n" + push)
+    else:
+        print('用户名和密码数量不对')
